1c07d30162c9c6c6b30dc1c86f9b2985383fcefc
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-02-23 15:58:22 +0000

    all site functional

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -11,13 +11,66 @@ router.post("/editPersonalProfile/:id/edit", async (req, res, next) => {
   try {
     const { id } = req.params;
     const { username, GOAT, password } = req.body;
+    let response = await User.findOne({ username: `${username}` });
+    let responseUser = await User.findById(id);
+    let responseAll = await User.find();
+    /* res.send(response) */
+    let check = false;
+    for (let i = 0; i < responseAll.length; i ++){
+        if(response.username === responseAll[i].username && response.username != responseUser.username){
+            check = true;
+        }
+    }
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}$/;
+    if (!username || !password || !GOAT) {
+      res.render("editPersonalProfile", {
+        errorMessage: "Please input all the fields",
+      });
+    } else if (!regex.test(password)) {
+      res.render("editPersonalProfile", {
+        errorMessage:
+          "Your password needs to be 4 characters long and include lowercase letters and uppercase letters",
+      });
+    } 
+    else if( GOAT != 'Michael Jordan' && GOAT != 'Lebron James'){
+        res.render("editPersonalProfile", {
+            errorMessage:
+              "You must choose between the only 2 GOAT's that exists, Michael Jordan or Lebron James 🏀",
+          });
+    }
+    else if (response.username === responseUser.username) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        await User.findByIdAndUpdate(id, {
+            username,
+            GOAT,
+            password: hashedPassword,
+        });
+        res.redirect(`/${id}/profile`);
+    } 
+    
+    else if(check){
+      res.redirect(`/${id}/profile`);        
+        res.render("editPersonalProfile", {
+            errorMessage:
+              "Username already registered",
+          });
+    } 
+      else{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        await User.findByIdAndUpdate(id, {
+            username,
+            GOAT,
+            password: hashedPassword,
+        });
+        res.redirect(`/${id}/profile`);
+    }
+    
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    await User.findByIdAndUpdate(id, { username, GOAT, password: hashedPassword });
-
-    res.redirect(`/${id}/profile`);
+    
   } catch (error) {
     console.log(error);
     next(error);
@@ -90,12 +143,12 @@ router.get("/:id/profile", async (req, res, next) => {
     next(error);
   }
 });
-router.get("/profile/:id", async (req, res, next) => {
+/* router.get("/profile/:id", async (req, res, next) => {
   try {
     let id = req.params.id;
     let user = await User.findById(id);
     res.render("profile", { user });
   } catch (error) {}
-});
+}); */
 
 module.exports = router;
